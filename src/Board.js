0f9da86c4b5b0a3d0abdd49a785fa03bf9711db0
Dmitrii Pashutskii <guar47@ya.ru>
2018-02-28 16:30:32 +0300

    add eslint, separate components, add location to descr

---
+++ src/Board.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
